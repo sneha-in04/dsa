f9a9619cfe7aa3e7a39eb1f0d9df3f41d0207a7c
sneha <guptasneha1393@gmail.com>
2024-05-05 12:32:03 +0530

    p2.java file added

---
+++ p2.java
@@ -0,0 +1,13 @@
+package dsa;
+
+public class p2 {
+    public static void main(String[] args) {
+        for(int i=1;i<6;i++){
+            for(int j=1;j<i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
